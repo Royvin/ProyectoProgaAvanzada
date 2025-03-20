ee50d67f1210cb59c885c0823a53489ecf1bbfa5
Royvin <royvin1205@gmail.com>
2025-03-19 21:26:52 -0600

    Commit Inicial

---
+++ ProyectoPograAvanzada/ProyectoPograAvanzada/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoPograAvanzada.MvcApplication" Language="C#" %>
---
+++ ProyectoPograAvanzada/ProyectoPograAvanzada/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoPograAvanzada.Startup))]
+namespace ProyectoPograAvanzada
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
