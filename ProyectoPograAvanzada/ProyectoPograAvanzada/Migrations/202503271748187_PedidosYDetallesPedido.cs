eaea32a862d80e86f13c7cbf6f0d050f27e3f674
Royvin <royvin1205@gmail.com>
2025-03-27 12:01:37 -0600

    Cambios after merge

---
+++ ProyectoPograAvanzada/ProyectoPograAvanzada/Migrations/202503271748187_PedidosYDetallesPedido.cs
@@ -0,0 +1,39 @@
+﻿namespace ProyectoPograAvanzada.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PedidosYDetallesPedido : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.DetallePedidos",
+                c => new
+                    {
+                        IdDetalle = c.Int(nullable: false, identity: true),
+                        IdPedido = c.Int(nullable: false),
+                        IdItem = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdDetalle);
+            
+            CreateTable(
+                "dbo.Pedidos",
+                c => new
+                    {
+                        IdPedido = c.Int(nullable: false, identity: true),
+                        idCarrito = c.Int(nullable: false),
+                        FechaCompra = c.DateTime(nullable: false),
+                        Estado = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdPedido);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Pedidos");
+            DropTable("dbo.DetallePedidos");
+        }
+    }
+}
